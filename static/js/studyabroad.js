8c3194038e13449b827e05fb5255e7645cdc527d
Mitchell <erich.mitchell87@gmail.com>
2021-04-20 19:36:58 -0600

    start of map

---
+++ static/js/studyabroad.js
@@ -0,0 +1,23 @@
+//set my map
+var myMap = L.map("map", {
+  center: [42.6611, -99.5302],
+  zoom: 4
+});
+
+// Adding a tile layer (the background map image) to our map
+// We use the addTo method to add objects to our map
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/streets-v11",
+  accessToken: API_KEY
+}).addTo(myMap);
+
+// var baseURL = '/data' 
+
+// d3.json(baseURL).then((data)=>{
+	
+// })
+// 	
