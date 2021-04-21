a17c9422792a32c6f503a769afea2e214ed24faa
Mitchell <erich.mitchell87@gmail.com>
2021-04-20 23:25:28 -0600

    inbound/outbound markers and map jump

--- static/js/studyabroad.js
+++ static/js/studyabroad.js
@@ -1,7 +1,8 @@
 //set my map
 var myMap = L.map("map", {
   center: [42.6611, -99.5302],
-  zoom: 4
+  zoom: 4,
+  worldCopyJump: true,
 });
 
 // Adding a tile layer (the background map image) to our map
@@ -15,36 +16,24 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
   accessToken: API_KEY
 }).addTo(myMap);
 
+L.geoJson()
+
+
 //read in data with var
 var university_url='http://127.0.0.1:5000/data'
 
 d3.json(university_url).then((response)=>{
     Object.entries(response).forEach((country)=>{
-      // console.log(value)
+      console.log(country)
       var lat = country[1]['cord']['latitude']
       var long = country[1]['cord']['longitude']
 
       L.marker([lat,long])
-      .bindPopup("<h1>" + country[0]+ "</h1> <hr> <h3> Inbound: " + country[1]['inbound']['in18/19'] + "</h3>")
+      .bindPopup("<h1>" + country[0]+ "</h1> <hr> <h3> Inbound: " + country[1]['inbound']['in18/19'] + "</h3> <h3> Outbound:" + country[1]['outbound']['out18/19']+"</h3>")
       .addTo(myMap);
+
+      // L.geoJson(country).addTo(myMap)
     })
 })
-
-
-
-//   // Add our marker cluster layer to the map
-//   myMap.addLayer(markers);
-
-// });  
-
-// for (var i= 0: i<university_url.length; i++) {
-//   var country = country[i];
-//   L.marker(country.cord)
-//     .bindPopup("<h1>"+country.value[0]+"</h1> <hr> <h3>"+value[0]['in18/19']+"</h3>")
-//     .addTo(myMap);
-// }
-
-
-
 
 
